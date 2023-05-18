55c9f8e6a5d32716261ffac17ec9823bc904592c
Victor Ponce <vicmanuelr@gmail.com>
2023-05-17 21:56:08 -0600

    Added sqlalchemy to flask app and modeled database with movies entries

--- main.py
+++ main.py
@@ -5,10 +5,38 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from sqlalchemy import exc
 
+# Basic flask application setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
+
+# Adding sqlalchemy object
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# this code is for the database model and table
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.name}>'
+
+
+with app.app_context():
+    try:
+        db.create_all()
+    except exc.IntegrityError:
+        pass
 
 
 @app.route("/")
